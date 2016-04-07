036bc955503608afb79288b7ab4c4a809a0916d4
MF57 <p-bochenek@outlook.com>
2016-04-07 16:55:39 +0200

    equivalent xml config

--- chapter2/src/test/java/com/spring/chapter2/CDPlayerXmlTest.java
+++ chapter2/src/test/java/com/spring/chapter2/CDPlayerXmlTest.java
@@ -8,14 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by mf57 on 07.04.2016.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = CDPlayerConfig.class)
-public class CDPlayerTest {
+@ContextConfiguration(locations = "classpath:cdPlayer.xml")
+public class CDPlayerXmlTest {
 
     @Autowired
     private CompactDisc cd;
